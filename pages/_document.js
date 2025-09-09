32d10230ed4efc743099b7573ac6be48a0110fd5
Snandi891 <nitunandi1985@gmail.com>
2025-09-08 22:37:57 -0700

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,17 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Google site verification */}
+        <meta
+          name="google-site-verification"
+          content="Rp8ch_A0AdO8zWFIkG7L_o2-h4hyRNeKVn5Vo8EZDwo"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
