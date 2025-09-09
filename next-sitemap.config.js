bf99e54c8d908c181ebdddde0deb9ad4f112f4dd
Snandi891 <nitunandi1985@gmail.com>
2025-09-08 22:50:21 -0700

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://gym-website-six-livid.vercel.app", // 🔴 Replace with your domain (or vercel URL)
+  generateRobotsTxt: true, // also generates robots.txt
+  sitemapSize: 7000, // splits sitemap if too many URLs
+};
